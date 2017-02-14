cf4fc991345789b633af92c99bb6a61dacd8e0ac
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2017-02-14 08:36:38 -0600

    generated false in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Lunch.delete_all
 Eater.delete_all
-Lunch.create(title: "initial lunch", date: "2017-1-1")
+Lunch.create(title: "initial lunch", date: "2017-1-1", generated: false)
 
 30.times do 
 	Eater.create(name: Faker::Name.name)
